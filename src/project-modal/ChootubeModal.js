2f9e25ffaf45fa685c0c0ffeda21cee9b21facce
jay sung <namic123@naver.com>
2024-01-03 11:26:29 +0900

    project modal 별도의 컴포넌트로 분리

--- src/project-modal/ChootubeModal.js
+++ src/project-modal/ChootubeModal.js
@@ -1,5 +1,59 @@
 import React from "react";
 
 export function ChootubeModal() {
-  return <></>;
+  return (
+    <>
+      <div className="modal-info">
+        <h2 className="modal-info-heading">Summary</h2>
+        <p className="modal-info-content">
+          늘 비슷한 것만 추천하는 유튜브 알고리즘에 한계를 느낀다. 나만 아는
+          재밌는, 유익한 유튜버를 모두에게 알리고 싶다. 나와 비슷한 관심사를
+          가진 사람들의 알고리즘이 궁금하다.
+        </p>
+        <br />
+        <h2 className="modal-info-heading">Overview</h2>
+        <p className="modal-info-content">
+          이 프로젝트는 유튜브 알고리즘의 한계를 극복하고 사용자 간의 고유한
+          취향과 관심사를 공유할 수 있는 커뮤니티 플랫폼입니다. 유튜브의 기존
+          추천 시스템이 종종 제한적이고 반복적인 콘텐츠를 제공하는 데 반해, 이
+          플랫폼은 다양하고 풍부한 콘텐츠 발굴을 촉진합니다. 사용자들은 자신만의
+          독특한 발견을 공유하고, 비슷한 관심사를 가진 다른 사용자들의 추천을
+          탐색할 수 있습니다. 이는 유튜브 시청 경험을 개인화하고, 커뮤니티
+          기반의 상호작용을 통해 새로운 콘텐츠를 발견하는 새로운 방식을
+          제공합니다.
+        </p>
+        <br />
+        <h2 className="modal-info-heading">Key Features</h2>
+        <br />
+        <h4> 개인화된 추천 공유</h4>
+        <p className="modal-info-content">
+          <span className="emphasis">*</span>사용자는 자신이 발견한 독특하고
+          흥미로운 유튜브 콘텐츠를 공유할 수 있습니다.
+        </p>
+        <h4> 관심사 기반 커뮤니티</h4>
+        <p className="modal-info-content">
+          <span className="emphasis">*</span>비슷한 관심사를 가진 사용자들과
+          정보를 교류하고 새로운 콘텐츠를 추천받을 수 있습니다.
+        </p>
+        <h4> 유저 중심의 인터페이스</h4>
+        <p className="modal-info-content">
+          <span className="emphasis">*</span>사용자 친화적인 디자인으로 누구나
+          쉽게 참여하고 추천을 공유할 수 있습니다.
+        </p>
+        <br />
+        <h2 className="modal-info-heading">Overview</h2>
+        <p className="modal-info-content">
+          이 프로젝트는 유튜브 알고리즘의 한계를 극복하고 사용자 간의 고유한
+          취향과 관심사를 공유할 수 있는 커뮤니티 플랫폼입니다. 유튜브의 기존
+          추천 시스템이 종종 제한적이고 반복적인 콘텐츠를 제공하는 데 반해, 이
+          플랫폼은 다양하고 풍부한 콘텐츠 발굴을 촉진합니다. 사용자들은 자신만의
+          독특한 발견을 공유하고, 비슷한 관심사를 가진 다른 사용자들의 추천을
+          탐색할 수 있습니다. 이는 유튜브 시청 경험을 개인화하고, 커뮤니티
+          기반의 상호작용을 통해 새로운 콘텐츠를 발견하는 새로운 방식을
+          제공합니다.
+        </p>
+      </div>
+      <div className="modal-info">바이</div>
+    </>
+  );
 }
